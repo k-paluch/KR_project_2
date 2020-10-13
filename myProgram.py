a2a4b1f26f1a4cdba11d59b261b3f79ad406665e
k-paluch <k.paluch@student.vu.nl>
2020-10-13 09:13:05 +0200

    heuristics update

--- myProgram.py
+++ myProgram.py
@@ -1,10 +1,14 @@
 import os
-from random import randint, seed
+from random import randint, seed,shuffle
+import time
+
+
+
 seed(1)
 # This is an example puython programme which shows how to use the different stand-alone versions of OWL reasoners and forgetting programme
 
 # Choose the ontology (in the OWL format) for which you want to explain the entailed subsumption relations.
-inputOntology = "./mamo-xml.owl"
+inputOntology = "./ModSci.owl"
 
 # Choose the set of subclass for which you want to find an explanation.
 # this file can be generated using the second command (saveAllSubClasses)
@@ -19,7 +23,8 @@ forgetOntology = "./result.owl"
 # 1 - ALCHTBoxForgetter
 # 2 - SHQTBoxForgetter
 # 3 - ALCOntologyForgetter
-method = "2" #
+
+method = "1" #
 
 # Choose the symbols which you want to forget.
 signature = "./signature.txt"
@@ -48,25 +53,46 @@ os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + inputOntol
 # --> uncomment the following line to run this function
 # os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + inputOntology + ' --method ' + method  + ' --signature ' + signature)
 
+heur1 = {}
 elements = []
+
 with open("subClasses.nt", "r") as file:
 	for line in file:
 		words = line.split()
 		if(words[0][1:-1] not in elements):
+			heur1.update({words[0][1:-1]: 1})
 			elements.append(words[0][1:-1])
+		else:
+			heur1.update({words[0][1:-1]: heur1[words[0][1:-1]]+1})
 		if(words[2][1:-1] not in elements):
+			heur1.update({words[2][1:-1]: 1})
 			elements.append(words[2][1:-1])
+		else:
+			heur1.update({words[2][1:-1]: heur1[words[2][1:-1]]+1})
 
-print(len(elements))
+
+heur1 = sorted(heur1.items(), key=lambda kv: kv[1])
+tmp = []
+for x in heur1:
+	tmp.append(x[0])
+# t = [k  for  k in  heur1.keys()]
+heur2 = shuffle(elements)
+
+# set heuristic
+elements = tmp
 
 to_forget = []
 
-for x in range(90):
-	tmp = randint(0,len(elements)-1)
-	to_forget.append(elements[tmp])
-	elements.pop(tmp)
+for x in reversed(elements):
+	# tmp = randint(0,len(elements)-1)
+	if(x!='https://w3id.org/skgo/modsci#Bioinformatics' and x!= 'https://w3id.org/skgo/modsci#Biology'):
+		to_forget.append(x)
+		print(x)
+		elements.pop(elements.index(x))
 
 first = 0
+
+n = 0
 
 for t in to_forget:
 	with open("signature.txt", "w") as f:
@@ -76,10 +102,13 @@ for t in to_forget:
 		first+=1
 	else:
 		os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + forgetOntology + ' --method ' + method  + ' --signature ' + signature)
+	n+=1
+	# os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + forgetOntology)
+	# os.system('java -jar kr_functions.jar ' + 'saveAllExplanations' + " " + forgetOntology + " " + inputSubclassStatements)
+	# time.sleep(5)
+	print(n)
 
-
-# os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + forgetOntology)
-os.system('java -jar kr_functions.jar ' + 'saveAllExplanations' + " " + forgetOntology + " " + inputSubclassStatements)
-
+os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + forgetOntology)
+os.system('java -jar kr_functions.jar ' + 'saveAllExplanations' + " " + inputOntology + " " + inputSubclassStatements)
 
 print(elements)
